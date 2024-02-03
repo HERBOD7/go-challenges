e89140eaa3b366b253c88c940028a5a06bf35ede
HERBOD7 <herbodabdoli@gmail.com>
2024-02-03 19:05:35 +0330

    feat: add jam challenge

---
+++ quera/jam.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	jarAndCap := strings.Split(scanner.Text(), " ")
+
+	scanner.Scan()
+	jams := strings.Split(scanner.Text(), " ")
+
+	jarCount, _ := strconv.Atoi(jarAndCap[0])
+	capacity, _ := strconv.Atoi(jarAndCap[1])
+
+	totalCap := jarCount * capacity
+	totalJam := 0
+
+	for _, value := range jams {
+		intValue, _ := strconv.Atoi(value)
+		totalJam += intValue
+	}
+
+	remainCap := totalCap - totalJam
+	remainJar := remainCap / capacity
+	fmt.Println(remainJar)
+}
