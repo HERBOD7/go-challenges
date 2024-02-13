3fdf54ba3da8227c40e635d175ecbf36af1d78e4
HERBOD7 <herbodabdoli@gmail.com>
2024-02-13 15:28:54 +0330

    fix: fix initial value

--- quera/simple.go
+++ quera/simple.go
@@ -10,17 +10,21 @@ import (
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	var (
-		sum,
-		maxInput float64
-	)
-
-	multiple := 1.0
-	minInput := 1.0
+	inputs := []float64{}
 
 	for i := 0; i < 4; i++ {
 		scanner.Scan()
 		input, _ := strconv.ParseFloat(scanner.Text(), 64)
+		inputs = append(inputs, input)
+	}
+
+	var sum float64
+	multiple := 1.0
+	minInput := inputs[0]
+	maxInput := inputs[0]
+
+	for i := 0; i < len(inputs); i++ {
+		input := inputs[i]
 		sum += input
 		multiple *= input
 		if input < minInput {
