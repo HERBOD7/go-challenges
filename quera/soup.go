0be2af7dcec734717c4b136d6a8e77574704799c
HERBOD7 <herbodabdoli@gmail.com>
2024-02-19 15:42:39 +0330

    feat: add quera challenge - 72881

--- quera/soup.go
+++ quera/soup.go
@@ -12,11 +12,11 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	inputs := strings.Split(scanner.Text(), " ")
-	n, _ := strconv.Atoi(inputs[0])
-	k, _ := strconv.Atoi(inputs[1])
-	s, _ := strconv.Atoi(inputs[2])
+	n, _ := strconv.ParseFloat(inputs[0], 64)
+	k, _ := strconv.ParseFloat(inputs[1], 64)
+	s, _ := strconv.ParseFloat(inputs[2], 64)
 
-	if n*k == s {
+	if n*k <= s {
 		fmt.Println("Kafie!")
 	} else {
 		fmt.Println("Na! yeki bayad bere sabzi bekhare")
